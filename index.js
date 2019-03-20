303b7200c0234b372f8afeeaa2abb129e24677cf
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 22:36:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  newvar=[]
+  var newvar=[]
   for(var i = 0; i<musicians.length; i++){
    var firstmusician = musicians[i];
    var firstinstrument = instruments[i];
