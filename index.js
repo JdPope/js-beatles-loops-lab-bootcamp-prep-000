0811e7eaa834395cb77cb566d735f57ee58f304d
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 22:41:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newvar=[]
   for(var i = 0; i<musicians.length; i++){
-   var firstmusician = musicians[i];
-   var firstinstrument = instruments[i];
+   var musician = musicians[i];
+   var instrument = instruments[i];
    newvar.push(firstmusician + " plays " + firstinstrument)
 }return newvar}
